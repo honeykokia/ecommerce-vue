f5c35e71da81e4dde54faebc14df8ea1434605ca
Strawberry <honeykokia@gmail.com>
2025-07-29 21:19:34 +0800

    feat: Add core UI components for homepage - `Advertisement.vue`: used for ad display - `FeatureList.vue`: show feature or new products - `Home`: Main homepage layout - `Navbar`: Top navigation bar

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Advertisement from './Advertisement.vue';
+import FeatureList from './FeatureList.vue';
+
+</script>
+
+<template>
+  <section class="advertisement">
+    <Advertisement />
+  </section>
+
+  <section class="featureList">
+    <FeatureList />
+  </section>
+</template>
+
+<style scoped>
+.advertisement {
+  @apply w-full p-6 bg-gradient-to-r from-blue-400 to-blue-600 flex items-center;
+}
+
+.featureList{
+  @apply w-full mx-auto p-6 bg-gray-200;
+}
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+  <div class="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
+    <h1 class="text-2xl font-bold text-center md:text-3xl md:text-left">
+      Welcome to My E-commerce Site
+    </h1>
+    <ul class="flex flex-wrap justify-center md:justify-end items-center gap-6">
+      <li>
+        <RouterLink
+          to="/"
+          class="text-black-500 hover:text-blue-700 border-b-2 border-transparent hover:border-blue-700"
+          ><i class="fa-solid fa-house text-2xl"></i
+        ></RouterLink>
+      </li>
+      <li>
+        <a
+          href="#search"
+          class="text-black-500 hover:text-blue-700 border-b-2 border-transparent hover:border-blue-700"
+          ><i class="fa-solid fa-magnifying-glass text-2xl"></i
+        ></a>
+      </li>
+      <li>
+        <a
+          href="#cart"
+          class="text-black-500 hover:text-blue-700 border-b-2 border-transparent hover:border-blue-700"
+          ><i class="fa-solid fa-cart-shopping text-2xl"></i
+        ></a>
+      </li>
+      <li>
+        <RouterLink
+          to="/login"
+          class="bg-gray-200 border-2 p-2 rounded-xl text-black-500 hover:text-blue-700 border-b-2 border-transparent hover:border-blue-700"
+          ><i class="fa-solid fa-user text-2xl"></i> 登入/註冊</RouterLink
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
